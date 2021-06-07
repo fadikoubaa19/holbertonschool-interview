748682b59807c2705d8cca44ec2ee3b661e34ef2
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-06-07 13:02:27 +0100

    lockboxes file upd

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,11 +6,11 @@ def canUnlockAll(boxes):
     lockboxes function
     """
 
-    ln = len(boxes)
+    n = len(boxes)
     bx = boxes[0]
-    bloc1 = [False] + [True] * (ln - 1)
+    bloc1 = [False] + [True] * (n - 1)
     for i in bx:
-        if ((i < ln) and (bloc1[i] is True)):
+        if ((i < n) and (bloc1[i] is True)):
             bloc1[i] = False
             bx.extend(boxes[i])
     return not any(bloc1)
