bec57147fdfa2c20b034ffe2946fa1b3526d9870
fadikoubaa19 <2062@holbertonschool.com>
2021-10-07 11:44:28 +0100

    upd name

---
+++ 0x0F-starwars_api/0-starwars_characteres.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, async (error, response, body) => {
+  if (movieId == null) return;
+  if (error) console.log(error);
+
+  for (let i = 0; i < JSON.parse(body).characters.length; i++) {
+    await new Promise((resolve, reject) => {
+      request(JSON.parse(body).characters[i], function (error, response, body) {
+        if (error) console.log(error);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
