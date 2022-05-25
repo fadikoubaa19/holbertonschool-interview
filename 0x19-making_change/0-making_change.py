ed58a48623a53974e3330797eb3ac604a67424b5
fadikoubaa19 <fadi.koubaa@gmail.com>
2022-05-25 15:24:40 +0100

    fixing

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,17 +5,13 @@
 def makeChange(coins, total):
     """function to determine the fewest number of coin"""
 
-    nc = 0
-    bg = 0
-    if total <= 0:
-        return 0
-
+    cal = 0
     coins = sorted(coins, reverse=True)
-
     for ele in coins:
-        while bg + ele <= total:
-            bg += ele
-            nc = nc + 1
-        if bg == total:
-            return nc
-    return -1
+        if total or ele != 0:
+            cal += total // ele
+            total = total % ele
+    if total == 0:
+        return cal
+    else:
+        return -1
