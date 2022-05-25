c3ca7ba174628690d0963b64a7a142a0ad859cc5
fadikoubaa19 <fadi.koubaa@gmail.com>
2022-05-25 15:13:57 +0100

    determine the coins function

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+""" task 0 """
+
+
+def makeChange(coins, total):
+    """function to determine the fewest number of coin"""
+
+    nc = 0
+    bg = 0
+    if total <= 0:
+        return 0
+
+    coins = sorted(coins, reverse=True)
+
+    for ele in coins:
+        while bg + ele <= total:
+            bg += ele
+            nc = nc + 1
+        if bg == total:
+            return nc
+    return -1
